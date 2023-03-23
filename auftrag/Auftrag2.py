60334eb09b157fc1dc90a594546989f803f2af7a
myuzkii <soezel@taskit.de>
2023-03-23 08:13:53 +0100

    BIG-AG angefangen

---
+++ auftrag/Auftrag2.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+code = np.array([1 ,0,0,0, 1,1 ,0,0,0, 1,1,1 ,0,0,0, 1,1,1,1])
+# code = np.array([
+#     1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1,
+#     0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0,
+#     1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1,
+#     1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1])
+
+pixel_per_bar = 4
+dpi = 100
+
+fig = plt.figure(figsize=(len(code) * pixel_per_bar / dpi, 2), dpi=dpi)
+ax = fig.add_axes([0, 0, 1, 1])  # span the whole figure
+ax.set_axis_off()
+ax.imshow(code.reshape(1, -1), cmap='binary', aspect='auto',
+          interpolation='nearest')
+
+plt.gcf()
+plt.savefig('Kundenauftrag2.png',dpi=30)
+plt.show()
---
+++ auftrag/trainingsworkshop.py
@@ -0,0 +1,12 @@
+# http://85.214.241.203/
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 1000)
+
+plt.plot(x)
+
+plt.gcf()
+plt.savefig('Kundenauftrag0.png',dpi=30)
+plt.show()
