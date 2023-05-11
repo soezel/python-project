53b619bb80d552171eadb4c21608e5be42741a1c
tamaraspringer <tspringer@taskit.de>
2023-05-11 10:30:15 +0200

    lage für das spiel wurde erstellt.

--- projektarbeit/main.py
+++ projektarbeit/main.py
@@ -1,17 +1,51 @@
-import pgzrun
+import pygame
+import sys
 
-WIDTH = 640
-HEIGHT = 480
+pygame.init()
 
-# Laden Sie das GIF-Image als Animation
-char_anim = pgzrun("projektarbeit/assets/luma.gif")
+CLOCK = pygame.time.Clock()
+SCREEN = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Frenzy: Battle Against Octopods Invasion")
 
-# Definieren Sie die Startposition des Charakters
-char_pos = (100, 100)
+X_POSITION, Y_POSITION = 400, 660
 
-def draw():
-    # Zeichnen Sie den Charakter
-    char_anim.draw(char_pos)
+jumping = False
 
-# Starten Sie das Spiel
-pgzrun.go()
+Y_GRAVITY = 0.6
+JUMP_HEIGHT = 20
+Y_VELOCITY = JUMP_HEIGHT
+
+STANDING_SURFACE = pygame.transform.scale(pygame.image.load("projektarbeit/assets/luma1.png"), (48, 64))
+JUMPING_SURFACE = pygame.transform.scale(pygame.image.load("projektarbeit/assets/luma2.png"), (48, 64))
+BACKGROUND = pygame.image.load("projektarbeit/assets/background.png")
+
+rect = STANDING_SURFACE.get_rect(center=(X_POSITION, Y_POSITION))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    keys_pressed = pygame.key.get_pressed()
+
+    if keys_pressed[pygame.K_SPACE]:
+        jumping = True
+
+    SCREEN.blit(BACKGROUND, (0, 0))
+    
+    if jumping:
+        Y_POSITION -= Y_VELOCITY
+        Y_VELOCITY -= Y_GRAVITY
+        if Y_VELOCITY < -JUMP_HEIGHT:
+            jumping = False
+            Y_VELOCITY = JUMP_HEIGHT
+        mario_rect = JUMPING_SURFACE.get_rect(center=(X_POSITION, Y_POSITION))
+        SCREEN.blit(JUMPING_SURFACE, mario_rect)
+    else:
+        mario_rect = STANDING_SURFACE.get_rect(center=(X_POSITION, Y_POSITION))
+        SCREEN.blit(STANDING_SURFACE, mario_rect)
+        
+
+    pygame.display.update()
+    CLOCK.tick(60)
